ae7f3c9cae1f2065f5ca9b4af2d18f60f84979da
levinh-3105 <levinh3105@gmail.com>
2026-06-24 17:10:18 +0700

    added mux

---
+++ rtl/scale_mux.sv
@@ -0,0 +1,18 @@
+module scale_mux #(
+    parameter WIDTH = 1
+) (
+    input [WIDTH-1:0] in_a, in_b,
+    input sel_a,
+    output logic [WIDTH-1:0] out
+);
+    timeunit 1ns;
+    timeprecision 100ps;
+
+    always_comb begin : mux_logic
+        out = '0;
+        case(sel_a)
+            1'b0: out = in_b;
+            1'b1: out = in_a;
+        endcase
+    end
+endmodule
